0a9f801d9fdb42b550ef7dbbe85ad126351ce1f0
James Kelly <jamesk@uber.com>
2020-04-03 16:52:23 +0100

    Added explicit wait loop to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_start_retrieve_list(self):
         # User navigates to to-do list homepage
@@ -39,8 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # User presses enter, the page now lists '1: Buy test item 1'
         input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_for_row_in_list_table('1: Buy test item 1')
+        self.wait_for_row_in_list_table('1: Buy test item 1')
 
         # User is still presented with a prompt to add a to-do item
         # User types 'Buy test item 2' into a text box
@@ -48,9 +58,8 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Buy test item 2')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.check_for_row_in_list_table('1: Buy test item 1')
-        self.check_for_row_in_list_table('2: Buy test item 2')
+        self.wait_for_row_in_list_table('1: Buy test item 1')
+        self.wait_for_row_in_list_table('2: Buy test item 2')
 
         # User confirms that the site has generated a unique url
 
