fdac6da552d1e168181e46a9cca1e5799a4ebd96
James Kelly <jamesk@uber.com>
2020-04-09 17:55:45 +0100

    debugging for .env file

--- superlists/settings.py
+++ superlists/settings.py
@@ -15,11 +15,15 @@ import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_FILE = BASE_DIR + '/.env'
+
+print('Env File Location:', ENV_FILE)
 
 env = environ.Env(
     DEBUG=(bool, False)
 )
-environ.Env.read_env(env_file=BASE_DIR + '/.env')
+
+environ.Env.read_env(env_file=ENV_FILE)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
