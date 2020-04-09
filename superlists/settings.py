976aaaf11133c4cc5d0e5aa02b9eb873ea3fb585
James Kelly <jamesk@uber.com>
2020-04-09 16:32:41 +0100

    moved .env to root directory and amended settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -13,14 +13,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(env_file=BASE_DIR + '/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
